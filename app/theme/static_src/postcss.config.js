5dea67036fbffc15c90d4eb686a65a7ebbcb8777
karim laayoun <karimacc001@gmail.com>
2020-01-23 17:20:59 +0100

    added webpack config and postcss-purgecss

--- app/theme/static_src/postcss.config.js
+++ app/theme/static_src/postcss.config.js
@@ -1,7 +1,15 @@
+const purgecss = require('@fullhuman/postcss-purgecss')
+const cssnano = require('cssnano')
+
 module.exports = {
   syntax: 'postcss-scss',
   plugins: [
     require('tailwindcss'),
     require('autoprefixer'),
+    cssnano({preset: 'default'}),
+    purgecss({
+      content: ['../templates/**/*.html'],
+      defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+    })
   ]
 }
