56107f73f54730ae1339dbfa15708a27b19c8f08
karim laayoun <karimacc001@gmail.com>
2020-01-28 00:17:44 +0100

    added svelte for the profile page

--- app/theme/static_src/webpack.config.js
+++ app/theme/static_src/webpack.config.js
@@ -1,9 +1,28 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, '..', 'static', 'js'),
+  entry: {
+    main: "./src/main/index.js",
+    svelte: "./src/svelte/index.js"
   },
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "..", "static", "js")
+  },
+  resolve: {
+    alias: {
+      svelte: path.resolve("node_modules", "svelte")
+    },
+    extensions: [".mjs", ".js", ".svelte"],
+    mainFields: ["svelte", "browser", "module", "main"]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(html|svelte)$/,
+        exclude: /node_modules/,
+        use: "svelte-loader"
+      }
+    ]
+  }
 };
