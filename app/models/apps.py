1dbabc4a634453536398e89d50672db4519ab853
sormaon0 <karimacc001@gmail.com>
2022-01-08 16:46:14 +0100

    upgrade django and djangorestframework

--- app/models/apps.py
+++ app/models/apps.py
@@ -3,6 +3,7 @@ from django.apps import AppConfig
 
 class ModelsConfig(AppConfig):
     name = 'models'
+    default_auto_field = 'django.db.models.BigAutoField'
 
     def ready(self):
         import models.signals
